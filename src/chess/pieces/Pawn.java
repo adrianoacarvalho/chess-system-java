64401069a54b9ede8d0eca1bfbf930a62444ccf8
adrianoacarvalho <acarvalho.software@gmail.com>
2025-07-11 23:38:15 -0300

    Bishop

--- src/chess/pieces/Pawn.java
+++ src/chess/pieces/Pawn.java
@@ -31,8 +31,8 @@ public class Pawn extends ChessPiece {
 			}
 			p.setValues(position.getRow() - 2, position.getColumn());
 			Position p2 = new Position(position.getRow() - 1, position.getColumn());
-			if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p) && getMoveCount() == 0
-					&& getBoard().positionExists(p2) && !getBoard().thereIsAPiece(p2)) {
+			if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p) 
+					&& getBoard().positionExists(p2) && !getBoard().thereIsAPiece(p2) && getMoveCount() == 0) {
 				mat[p.getRow()][p.getColumn()] = true;
 			}
 			
@@ -54,8 +54,8 @@ public class Pawn extends ChessPiece {
 			}
 			p.setValues(position.getRow() + 2, position.getColumn());
 			Position p2 = new Position(position.getRow() + 1, position.getColumn());
-			if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p) && getMoveCount() == 0
-					&& getBoard().positionExists(p2) && !getBoard().thereIsAPiece(p2)) {
+			if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p) 
+					&& getBoard().positionExists(p2) && !getBoard().thereIsAPiece(p2) && getMoveCount() == 0) {
 				mat[p.getRow()][p.getColumn()] = true;
 			}
 			
